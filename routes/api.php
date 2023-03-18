9f888076e2aaef4acc680cb74d215b99fd58c2a1
ariya <ariyapnm@gmail.com>
2023-03-19 03:25:51 +0700

    membuat api show register dan melihat dari ID

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,6 @@ Route::post('register',[AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
 Route::middleware('admin.api')->prefix('admin')->group (function () {
     Route::post('register',[AdminController::class,'register']);
+    Route::get('register',[AdminController::class,'show_register']);
+    Route::get('register/{id}',[AdminController::class,'show_register_by_id']);
 });

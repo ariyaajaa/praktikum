8274bad37f7f0d4a2fe0252e396bcffe2e4e74e6
ariya <ariyapnm@gmail.com>
2023-03-20 11:14:18 +0700

    membuat recipe

---
+++ app/Models/recipe.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class recipe extends Model
+{
+    use HasFactory;
+    protected $table='bahan';
+    protected $fillable=['nama','satuan','banyak','keterangan','resep_idresep'];
+}
